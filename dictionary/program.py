45477f730f1257273deaa9ea4fab1dbb90cbcb0b
Eileen Ai <eileen.aix@gmail.com>
2019-08-14 18:08:19 -0400

    wip

--- dictionary/program.py
+++ dictionary/program.py
@@ -1,10 +1,13 @@
 import json
+import difflib
 
 data = json.load(open("data.json"))
 
 
 def translate():
     x = input("What would you like to look up? ")
+    x = x.lower()
+    x = difflib.get_close_matches(x, data.keys(), n=1)
     if x in data:
         return data[x]
     else:
@@ -13,3 +16,6 @@ def translate():
 
 
 print(translate())
+
+# x = difflib.get_close_matches("there is ej gnsdf", data)
+# print(x[0])
