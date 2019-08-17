59fd73a63fc45508f0110f4d1a7a2d14cc72aaa3
Eileen Ai <eileen.aix@gmail.com>
2019-08-16 20:14:11 -0400

    finished

--- dictionary/program.py
+++ dictionary/program.py
@@ -9,18 +9,30 @@ def translate(word):
     close_match = get_close_matches(word, data.keys())
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     elif len(close_match) > 0:
         print("Did you mean %s instead?" % close_match[0])
         user_confirmation = input("Y or N\n")
         if user_confirmation == 'Y':
             return data[close_match[0]]
+        elif user_confirmation == 'N':
+            return "The word doesn't exist. Please double check."
         else:
-            return "Please try again."
+            return "The word doesn't exist. Please try again"
 
     else:
-        print("Word not found. Please enter again.")
-        translate()
+        return "Word not found. Please enter again."
 
 
 x = input("What would you like to look up? ")
-print(translate(x))
+
+output = translate(x)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
